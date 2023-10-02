55a32c1ee5abf8611dfab99f277dc8a7427b9d88
maxim12313 <maximkim16@gmail.com>
2023-10-02 19:55:45 -0400

    added istringstream format

--- printToString.cpp
+++ printToString.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     vector<string> output1;
-    string output2 = "\"";
+    vector<string> output2;
     string next;
     while (getline(cin, next)) {
         if (next == "stop") break;
@@ -12,16 +12,23 @@ int main() {
         adder.append(next).append("\\n\"");
         output1.push_back(adder);
 
-        output2.append(next).append("\\n");
+        string adder2 = "\"";
+        adder2.append(next).append("\"");
+        output2.push_back(adder2);
         // cout << "output: " << output << endl;
     }
-
-    output2.append("\"");
-    
     cout << "----------ostringstream output------------" << endl;
     for (int i = 0; i < output1.size(); i++) {
         const string &val = output1[i];
         if (i == output1.size() - 1) 
+            cout << val << ";" << endl;
+        else
+            cout << val << endl;
+    }
+    cout << "----------istringstream input-------------" << endl;
+    for (int i = 0; i < output2.size(); i++) {
+        const string &val = output2[i];
+        if (i == output2.size() - 1) 
             cout << val << ";" << endl;
         else
             cout << val << endl;
